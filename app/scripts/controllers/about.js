e2f16451e9a720359a22bbe34afcf556d3f36ab0
javier <js@silveriofunk.com>
2014-09-22 23:04:19 +0100

    initial app

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name firstYeomanAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the firstYeomanAppApp
+ */
+angular.module('firstYeomanAppApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name firstYeomanAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the firstYeomanAppApp
+ */
+angular.module('firstYeomanAppApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
