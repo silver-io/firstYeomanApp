40c18153a6d08cffbe8fd05aa569e1c1cb4f5c45
javier <js@silveriofunk.com>
2014-09-23 12:26:31 +0100

    can now add todo's

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,9 @@
  */
 angular.module('firstYeomanAppApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    $scope.todos = ['Item 1', 'Item 2', 'Item 3'];
+    $scope.addTodo = function () {
+  		$scope.todos.push($scope.todo);
+  		$scope.todo = '';
+};
   });
